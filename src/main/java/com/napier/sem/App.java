8140d24ab4f7b53b345bc94c7e801cbaa51926a9
PaingThetKyaw <40632014@live.napier.ac.uk>
2024-01-30 11:24:26 +0630

    Added new code that can print all the salaries obtained from getAllSalaries()

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -197,6 +197,23 @@ public class App
             return null;
         }
     }
+    /**
+     * Prints a list of employees.
+     * @param employees The list of employees to print.
+     */
+    public static void printSalaries(ArrayList<Employee> employees)
+    {
+        // Print header
+        System.out.println(String.format("%-10s %-15s %-20s %-8s", "Emp No", "First Name", "Last Name", "Salary"));
+        // Loop over all employees in the list
+        for (Employee emp : employees)
+        {
+            String emp_string =
+                    String.format("%-10s %-15s %-20s %-8s",
+                            emp.emp_no, emp.first_name, emp.last_name, emp.salary);
+            System.out.println(emp_string);
+        }
+    }
     public static void main(String[] args)
     {
         // Create new Application
@@ -211,8 +228,12 @@ public class App
         // Extract employee salary information
         ArrayList<Employee> employees = a.getAllSalaries();
 
+
         // Test the size of the returned data - should be 240124
-        System.out.println(employees.size());
+        System.out.println("Array Size: "+ employees.size() + "\n");
+
+        // Prints all the Salaries obtained from getAllSalaries()
+        // printSalaries(employees);
 
         // Disconnect from database
         a.disconnect();
